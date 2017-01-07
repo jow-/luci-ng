211935fc0e8f83f2834949fe795258eb18e16b18
Adrian Panella <ianchi74@outlook.com>
2017-01-06 18:20:09 -0600

    luci2-ui-base: build: add gulp tasks
    Add Gulp tasks to automate the building:
    + fetch and wire external libs dependencies
    + automatically instert <scripts> and <style> tags
    + concatenate all files for distribution
    
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- luci2-ui-base/src/gulpfile.js
+++ luci2-ui-base/src/gulpfile.js
@@ -6,7 +6,7 @@ const path = require('path');
 const del = require('del');
 const proxy = require('http-proxy-middleware');
 const $ = require('gulp-load-plugins')(); //load all gulp-* modules
-
+$.wiredep = require('wiredep').stream;
 // load configuration files
 const pkg = require('./package.json');
 const conf = require('./gulp.json');
@@ -18,30 +18,49 @@ gulp.task('clean', (done) => {
 	done();
 });
 
-// build: trivial copy (stub)
-gulp.task('build', (done) => {
-	gulp.src(['www/**/*'])
-	.pipe(gulp.dest('dist'));
-	done();
-});
+
+gulp.task(inject);
+
+gulp.task('build', gulp.parallel(copyFiles, gulp.series('inject', build)));
+
+gulp.task('serve', gulp.series('inject', watch, serveDev));
+gulp.task('serve:dist', gulp.series('build', serveDist));
+
 
 // serve: web server with proxy, for development
-gulp.task('serve', () => {
-		browserSync.init({
-			server: {
-				baseDir: ['www'],
-				index: 'luci-ng.html',
-				middleware: proxy('http://lede.lan/ubus')
+function serveDev() {
+	browserSync.init({
+		server: {
+			baseDir: [conf.paths.tmp, conf.paths.src],
+			index: conf.paths.mainHtml,
+			routes: {
+				'/bower_components': 'bower_components'
 			},
-			open: true
-		});
-});
+			middleware: proxy('http://lede.lan/ubus')
+		},
+		open: false
+	});
+}
+
+function serveDist() {
+	browserSync.init({
+		server: {
+			baseDir: [conf.paths.dist],
+			index: conf.paths.mainHtml,
+			middleware: proxy('http://lede.lan/ubus')
+		},
+		open: false
+	});
+}
 
 // lint: lint js files
 gulp.task('lint', () => {
-	return gulp.src(path.join(conf.paths.src, '**/*.js'))
-		.pipe($.eslint())
-		.pipe($.eslint.format());
+	return gulp.src([path.join(conf.paths.src, '**/*.js')])
+		.pipe($.eslint({
+			fix: true
+		}))
+		.pipe($.eslint.format())
+		.pipe(gulp.dest(conf.paths.tmp));
 });
 
 // js: concatenate & minify .js
@@ -60,8 +79,76 @@ gulp.task('js', () => {
 		.pipe(gulp.dest(conf.paths.tmp))
 		//.pipe($.filter('**/*.js'))
 		.pipe($.uglify())
-		.pipe($.rename({ extname: '.min.js' }))
+		.pipe($.rename({
+			extname: '.min.js'
+		}))
 		.pipe($.sourcemaps.write('maps'))
 		.pipe(gulp.dest(conf.paths.dist))
 		.pipe(gulp.dest(conf.paths.tmp));
 });
+
+
+function inject() {
+	const css = gulp.src(path.join(conf.paths.src, '**/*.css'), {
+		read: false
+	});
+	const js = gulp.src([
+		path.join(conf.paths.src, conf.paths.app, '*.js')
+  ], {
+		read: false
+	});
+
+	const opts = {
+		ignorePath: [conf.paths.src, conf.paths.tmp],
+		addRootSlash: false
+	};
+
+	return gulp.src(path.join(conf.paths.src, conf.paths.mainHtml))
+		.pipe($.inject(css, opts))
+		.pipe($.inject(js, opts))
+		.pipe($.wiredep(Object.assign({}, conf.wiredep)))
+		.pipe(gulp.dest(conf.paths.tmp))
+		.pipe(browserSync.stream());
+}
+
+
+function build() {
+	return gulp.src(path.join(conf.paths.tmp, conf.paths.mainHtml))
+		.pipe($.useref())
+		.pipe($.if('*.js_', $.ngAnnotate({
+			remove: true,
+			add: true,
+			single_quotes: true
+		})))
+		.pipe(gulp.dest(conf.paths.dist))
+		.pipe(browserSync.stream());
+}
+
+function copyFiles() {
+	return gulp.src([path.join(conf.paths.src, conf.paths.app, 'view/**/*'),
+									 path.join(conf.paths.src, conf.paths.app, 'controller/**/*'),
+									 path.join(conf.paths.src, conf.paths.app, 'icons/**/*'),
+									 path.join(conf.paths.src, conf.paths.app, 'proto/**/*'),
+									], {
+			base: conf.paths.src
+		})
+		.pipe(gulp.dest(conf.paths.dist));
+
+}
+
+
+function reloadBrowserSync(done) {
+  browserSync.reload();
+  done();
+}
+
+function watch(done) {
+  gulp.watch([path.join(conf.paths.src, conf.paths.mainHtml),
+							'bower.json',
+							path.join(conf.paths.src, '**/*.js'),
+						 ], 'inject');
+
+  //gulp.watch(conf.path.src('app/**/*.html'), gulp.series('partials', reloadBrowserSync));
+  //gulp.watch([conf.path.src('**/*.css')], gulp.series('styles'));
+  done();
+}
