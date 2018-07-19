10e7d5caada3fab7415fa3689daf8bf0931ae3f8
Adrian Panella <ianchi74@outlook.com>
2018-07-19 14:52:22 -0500

    ui-core: remove static views
    Removes statics views and prepares to loading views from
    json definitions using widgets
    
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- luci2-ui-core/src/src/app/app.module.ts
+++ luci2-ui-core/src/src/app/app.module.ts
@@ -8,51 +8,39 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router/router';
 
 import { AppComponent } from './app.component';
-import { StatusComponent } from './plugins/status/status.component';
-import { UbusViewerComponent } from './plugins/ubusViewer/ubusViewer.component';
-import { UciEditorComponent } from './plugins/uciEditor/uciEditor.component';
 import { JsonrpcService } from './shared/jsonrpc.service';
 import { MenuService } from './shell/menu/menu.service';
 import { ShellModule } from './shell/shell.module';
-import { UbusDirective } from './ubus/ubus.directive';
 import { UbusService } from './ubus/ubus.service';
-import { StatsComponent } from './widgets/stats/stats.component';
-import { UciModule } from 'app/uci/module';
+import { UciModule } from './uci/module';
 import { MaterialModule } from './material.module';
+import { WidgetsModule, RoutedWidgetComponent } from './widgets';
+import { ViewsResolverService } from './shared/viewsresolver.service';
+import { MenuGuardService } from './shell/menu/menuguard.service';
 
-const routes: Routes = [
-  { path: 'status', component: StatusComponent },
-  { path: 'system', component: UbusViewerComponent },
-  { path: 'network', component: UciEditorComponent },
-  { path: '', redirectTo: '/status', pathMatch: 'full' },
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UbusDirective,
-
-    StatusComponent,
-    UbusViewerComponent,
-    StatsComponent,
-    UciEditorComponent
-
-
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     ShellModule,
+    RouterModule.forRoot([{
+      path: '*', component: RoutedWidgetComponent, resolve: ViewsResolverService,
+      canActivate: [MenuGuardService]
+    }]),
     WidgetsModule,
 
     UciModule,
     MaterialModule
   ],
-  providers: [JsonrpcService, UbusService, MenuService ],
+  providers: [JsonrpcService, UbusService, MenuService],
+  entryComponents: [ RoutedWidgetComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
