a5baae31200b495fe17ce5660129fd1b44adff52
Adrian Panella <ianchi74@outlook.com>
2018-09-29 11:02:21 -0500

    ui-core: adds jsonPath support in Ubus calls and Expressions
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- luci2-ui-core/src/src/app/widgets/rootContext.ts
+++ luci2-ui-core/src/src/app/widgets/rootContext.ts
@@ -9,6 +9,7 @@ import { Context, ESpression, Expressions, ROOT_EXPR_CONTEXT } from 'reactive-js
 
 import { UbusService } from '../shared/ubus.service';
 import { UciModel2 } from '../uci/uci';
+import { JsonPath } from 'espression-jsonpath';
 
 export const rootContextProvider = {
   // tslint:disable-line:naming-convention
@@ -18,16 +19,18 @@ export const rootContextProvider = {
 };
 
 export function rootContextFactory(ubus: UbusService, expr: ESpression, uci: UciModel2): Context {
+  const jsonPath = new JsonPath();
   return Context.create(
     undefined,
     undefined,
     {
-      ubus: ubus.call.bind(ubus),
+      ubus: ubus.callFactory(),
       ubusList: ubus.list.bind(ubus),
       map: expr.mapFactory(),
       reduce: expr.reduceFactory(),
       uci,
       $user: ubus.user,
+      jsonPath: (obj: object, path: string) => jsonPath.query(obj, path).values
     },
     undefined,
     true
