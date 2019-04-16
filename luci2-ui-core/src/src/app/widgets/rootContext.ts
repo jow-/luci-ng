14a814ec7ba3d834605f9876a216afb3c29b10e5
Adrián Panella <ianchi74@outlook.com>
2019-04-16 09:20:48 -0500

    ui-core: expose snackbar to widgets
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- luci2-ui-core/src/src/app/widgets/rootContext.ts
+++ luci2-ui-core/src/src/app/widgets/rootContext.ts
@@ -6,11 +6,12 @@
  */
 
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
 import { JsonPath } from 'espression-jsonpath';
 import { RxObject } from 'espression-rx';
 import { Context, ESpression, Expressions, ROOT_EXPR_CONTEXT } from 'reactive-json-form-ng';
 import { of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, take } from 'rxjs/operators';
 
 import { UbusService } from '../shared/ubus.service';
 import { UciModel2 } from '../uci/uci';
@@ -19,14 +20,15 @@ export const rootContextProvider = {
   // tslint:disable-line:naming-convention
   provide: ROOT_EXPR_CONTEXT,
   useFactory: rootContextFactory,
-  deps: [UbusService, Expressions, UciModel2, HttpClient],
+  deps: [UbusService, Expressions, UciModel2, HttpClient, MatSnackBar],
 };
 
 export function rootContextFactory(
   ubus: UbusService,
   expr: ESpression,
   uci: UciModel2,
-  http: HttpClient
+  http: HttpClient,
+  snackbar: MatSnackBar
 ): Context {
   const jsonPath = new JsonPath();
   return Context.create(
@@ -41,6 +43,20 @@ export function rootContextFactory(
       $user: ubus.user,
       jsonPath: (obj: object, path: string) => jsonPath.query(obj, path).values,
       load: (url: string) => http.get(url).pipe(catchError(() => of(undefined))),
+      snackbar(message: string, action: string, onAction: string): true {
+        // use 'funtion' to have 'this' as the calling context
+
+        const snack = snackbar.open(message, action, { duration: 5000 });
+        if (onAction)
+          snack.onAction().subscribe(() => {
+            expr
+              .eval(onAction, this, true) // tslint:disable-line: no-invalid-this
+              .pipe(take(1))
+              .subscribe();
+          });
+
+        return true;
+      },
       RxObject,
       $tmp: RxObject({}),
     },
